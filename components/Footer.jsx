9578b045d867769e3db40d809eb1c5a4f02de133
Folareen <sakawahab03@gmail.com>
2022-09-27 22:11:58 +0100

    added footer component to layout

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+const Footer = () => {
+  return (
+    <Typography sx={{textAlign: 'center', borderTop: '1px solid grey', p: 0.5, fontFamily: 'Signika'}}>
+      Everything Football &copy; 2022
+    </Typography>
+  )
+}
+
+export default Footer
