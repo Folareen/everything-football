1eb98e384c6dfeeb99f8de73bfaee3627c78f309
Folareen <sakawahab03@gmail.com>
2022-10-10 11:03:30 +0100

    added head

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import ColorModeContextProvider from "../context/colorModeContext";
 import Layout from "../components/Layout";
 import "@fontsource/signika";
 import "@fontsource/kanit";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ColorModeContextProvider>
+      <Head>
+        <title>Everything Football</title>
+        <meta name="description" content="football app" />
+        <link rel="icon" href="/ball.svg" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
