3b2abe0faf7680eebf6978b6546691019f5dec2a
Folareen <sakawahab03@gmail.com>
2022-10-11 08:39:37 +0100

    completed standings

--- pages/standings/[standingId].js
+++ pages/standings/[standingId].js
@@ -1,81 +1,71 @@
-import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
 import StandingsNav from "../../components/StandingsNav";
 import Table from "../../components/Table";
+import { useRouter } from "next/router";
 
-const standing = ({ leagues, table }) => {
+const Standing = ({ leagues }) => {
+  const {
+    query: { standingId },
+  } = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    // (async () => {
+    //   try {
+    //     const options = {
+    //       method: "GET",
+    //       headers: {
+    //         "X-RapidAPI-Key":
+    //           "73b0945536msh8ea50af6e2d00b7p1388cejsnd67b8bc47222",
+    //         "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
+    //       },
+    //     };
+    //     const response = await fetch(
+    //       `https://transfermarket.p.rapidapi.com/competitions/get-table?id=${standingId}&seasonID=2022&domain=de`,
+    //       options
+    //     );
+    //     const data = await response.json();
+    //     setTableData(data.table);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // })();
+
+    setLoading(false);
+  }, [standingId]);
+
   return (
     <Box sx={{ pb: 2 }}>
       <StandingsNav leagues={leagues} />
-      <Table table={table} />
+      {loading && <Typography>Loading..</Typography>}
+      {tableData && !loading && <Table table={tableData} />}
     </Box>
   );
 };
 
-export default standing;
+export default Standing;
 
-export const getStaticPaths = async () => {
-  const seasonsOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a3f5923269mshb2b9a1649ef26b6p14ec30jsn84d8543adf73",
-      "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
-    },
-  };
+// export const getServerSideProps = async () => {
+//   const seasonsOptions = {
+//     method: "GET",
+//     headers: {
+//       "X-RapidAPI-Key": "73b0945536msh8ea50af6e2d00b7p1388cejsnd67b8bc47222",
+//       "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
+//     },
+//   };
 
-  const seasonResponse = await fetch(
-    "https://transfermarket.p.rapidapi.com/competitions/list-default?domain=de",
-    seasonsOptions
-  );
-  const season = await seasonResponse.json();
-
-  const paths = season.defaultCompetitions.map((league) => {
-    return {
-      params: {
-        standingId: league.id,
-      },
-    };
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async (context) => {
-  const { params } = context;
-  const seasonsOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a3f5923269mshb2b9a1649ef26b6p14ec30jsn84d8543adf73",
-      "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
-    },
-  };
-
-  const seasonResponse = await fetch(
-    "https://transfermarket.p.rapidapi.com/competitions/list-default?domain=de",
-    seasonsOptions
-  );
-  const season = await seasonResponse.json();
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a3f5923269mshb2b9a1649ef26b6p14ec30jsn84d8543adf73",
-      "X-RapidAPI-Host": "transfermarket.p.rapidapi.com",
-    },
-  };
-
-  const response = await fetch(
-    `https://transfermarket.p.rapidapi.com/competitions/get-table?id=${params.standingId}&seasonID=2022&domain=de`,
-    options
-  );
-  const tableResponse = await response.json();
-
-  return {
-    props: {
-      leagues: season.defaultCompetitions,
-      table: tableResponse.table,
-    },
-  };
-};
+//   const seasonResponse = await fetch(
+//     "https://transfermarket.p.rapidapi.com/competitions/list-default?domain=de",
+//     seasonsOptions
+//   );
+//   const season = await seasonResponse.json();
+//   return {
+//     props: {
+//       leagues: season.defaultCompetitions,
+//     },
+//   };
+// };
