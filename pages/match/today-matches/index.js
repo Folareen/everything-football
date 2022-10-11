5924d7610005db127769daacc34b1b3a5306b588
Folareen <sakawahab03@gmail.com>
2022-10-11 08:41:56 +0100

    completed today matches page

--- pages/match/today-matches/index.js
+++ pages/match/today-matches/index.js
@@ -1,41 +1,39 @@
-import Box from "@mui/material/Box";
-import TodayMatches from "../../../components/TodayMatches";
-import MatchesNav from "../../../components/MatchesNav";
+import { Box, Typography } from "@mui/material";
+import TodayMatches from "../../../components/Match/TodayMatches";
+import MatchesNav from "../../../components/Match/MatchesNav";
+import useSwr from "swr";
 
-const match = ({ todayData }) => {
+const Match = () => {
+  const { data, error } = useSwr("today-matches", async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "b46a755c58msh18b29b50c9486bfp1ff880jsna3d8784fac25",
+        "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
+      },
+    };
+
+    const date = new Date();
+    const fullDate = `${date.getFullYear()}${
+      date.getMonth() + 1
+    }${date.getDate()}`;
+
+    const response = await fetch(
+      `https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=soccer&Date=${fullDate}`,
+      options
+    );
+    const data = await response.json();
+
+    return data;
+  });
+
   return (
     <Box sx={{ width: "100%", pb: 2 }}>
       <MatchesNav />
-      <TodayMatches data={todayData} />
+      {!data && !error && <Typography>Loading..</Typography>}
+      {data && <TodayMatches data={data} />}
     </Box>
   );
 };
 
-export default match;
-
-export const getServerSideProps = async () => {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a3f5923269mshb2b9a1649ef26b6p14ec30jsn84d8543adf73",
-      "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
-    },
-  };
-
-  const date = new Date();
-  const fullDate = `${date.getFullYear()}${
-    date.getMonth() + 1
-  }${date.getDate()}`;
-
-  const todayResponse = await fetch(
-    `https://livescore6.p.rapidapi.com/matches/v2/list-by-date?Category=soccer&Date=${fullDate}`,
-    options
-  );
-  const todayData = await todayResponse.json();
-
-  return {
-    props: {
-      todayData,
-    },
-  };
-};
+export default Match;
