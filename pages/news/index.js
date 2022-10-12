44921b4e453453f897322004b0d94240fa871541
Saka Wahab Afolarin <58552881+Folareen@users.noreply.github.com>
2022-10-12 23:03:27 +0100

    changed api key

--- pages/news/index.js
+++ pages/news/index.js
@@ -14,7 +14,7 @@ const News = () => {
         method: "GET",
         headers: {
           "X-RapidAPI-Key":
-            "b46a755c58msh18b29b50c9486bfp1ff880jsna3d8784fac25",
+            "3dbacb8771msh387952f423fd831p1df808jsn772e2db53a42",
           "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
         },
       };
@@ -31,7 +31,7 @@ const News = () => {
         method: "GET",
         headers: {
           "X-RapidAPI-Key":
-            "b46a755c58msh18b29b50c9486bfp1ff880jsna3d8784fac25",
+            "3dbacb8771msh387952f423fd831p1df808jsn772e2db53a42",
           "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
         },
       };
